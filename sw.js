a4e060f95240cbd50a7b5bd0ad42b85ac6289071
GouravKesarkar <gourav.anand.kesarkar@gmail.com>
2026-09-21 05:50:30 +0000

    210920261120

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Bump CACHE when you deploy a new version so phones pick up the update.
+const CACHE = 'spend-sorter-v2';
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/maskable-512.png', './icons/apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+// Same-origin GETs: serve from cache instantly, refresh the cache in the background.
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  e.respondWith(
+    caches.match(req, {ignoreSearch: true}).then(hit => {
+      const fresh = fetch(req).then(res => {
+        if (res.ok) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); }
+        return res;
+      }).catch(() => hit);
+      return hit || fresh;
+    })
+  );
+});
