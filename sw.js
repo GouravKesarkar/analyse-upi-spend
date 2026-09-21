f2585eb8d653c60df176950aa93bec18f870bd22
GouravKesarkar <gourav.anand.kesarkar@gmail.com>
2026-09-21 06:38:20 +0000

    210920261210

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump CACHE when you deploy a new version so phones pick up the update.
 const CACHE = 'spend-sorter-v6';
-const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/maskable-512.png', './icons/apple-touch-icon.png'];
+const SHELL = ['./', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/maskable-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
